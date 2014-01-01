13a7b031d6b50c67ec4d90241dd50174086c9770
koenmetsu <koen.metsu@gmail.com>
2014-01-01 23:42:18 +0100

    test project setup

---
+++ DuckDuckGo.Net.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DuckDuckGo.Net.Tests
+{
+    [TestFixture]
+    public class Explorations
+    {
+        [Test]
+        public void BasicTest()
+        {
+        }
+    }
+}
