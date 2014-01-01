b361bb25fd84cb4a84e622d45729993d9967bb58
koenmetsu <koen.metsu@gmail.com>
2014-01-02 00:19:50 +0100

    added some basic exploratory tests

---
+++ DuckDuckGo.Net.Tests/Explorations.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace DuckDuckGo.Net.Tests
+{
+    [TestFixture]
+    public class Explorations
+    {
+        private const string ApplicationName = "DuckDuckGo.NET test project";
+
+        [Test]
+        public void FamousThingsHaveAnOfficialSite()
+        {
+            var result = new Search().Query("studio ghibli", ApplicationName);
+
+            Assert.AreEqual(result.Results.First().Text, "Official site");
+        }
+
+        [Test]
+        public void FamousThingsAlsoHaveAnAbstract()
+        {
+            var result = new Search().Query("studio ghibli", ApplicationName);
+
+            Assert.IsNotEmpty(result.Abstract);
+            Assert.IsNotEmpty(result.AbstractSource);
+            Assert.IsNotEmpty(result.AbstractUrl);
+            Assert.IsNotEmpty(result.AbstractText);
+        }
+
+        [Test]
+        public void SomeThingsHaveRelatedTopics()
+        {
+            var result = new Search().Query("black", ApplicationName);
+
+            Assert.Greater(result.RelatedTopics.Count, 1);
+        }
+
+        [Test]
+        public void SomeThingsHaveADefinition()
+        {
+            var result = new Search().Query("black", ApplicationName);
+
+            Assert.IsNotEmpty(result.Definition);
+            Assert.IsNotEmpty(result.DefinitionUrl);
+            Assert.IsNotEmpty(result.DefinitionSource);
+        }
+    }
+}
